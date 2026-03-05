34808f1b04b51e4eedd948976a2d30991cccb064
dezzy <desmond.monotone@gmail.com>
2026-03-05 11:55:08 +0000

    libAudioPluginSocillator.so built, unity project created

--- audioplugin/Oscillator.cpp
+++ audioplugin/Oscillator.cpp
@@ -2,12 +2,16 @@
 #include "AudioPluginInterface.h"
 #include "AudioPluginUtil.h"
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+
+
 enum Param
 {
     P_FREQ,
     P_NUM
 };
-
 //creates an interface to control the oscillator inside unity
 int InternalRegisterEffectDefinition(UnityAudioEffectDefinition& definition)
 {
@@ -19,11 +23,70 @@ int InternalRegisterEffectDefinition(UnityAudioEffectDefinition& definition)
         P_FREQ);
     return numparams;
 }
-
 struct OscData{
     //number of samples per second
     float sample_rate;
     //position in waveform, 0 - start of cycle, 0.5 - apex, 1 - back to the start
     float phase;
+    //current frequency
+    float freq;
 };
 
+//initialise oscillator
+UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK CreateCallback(UnityAudioEffectState* state){
+    OscData* data = new OscData();
+    data->sample_rate = state->samplerate; //48000hz 
+    data->phase = 0.0f;
+
+    state->effectdata = data;
+
+    return UNITY_AUDIODSP_OK;
+}
+
+
+UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ProcessCallback(
+    UnityAudioEffectState* state,
+    //incoming audio, can be ignored as the oscillator only produces audio
+    float* inbuffer,
+    //outgoing audio
+    float* outbuffer,
+    //no. of frames in the buffer
+    unsigned int length,
+    //input channels, can be ignored
+    int inchannels,
+    //output channels
+    int outchannels)
+{
+    OscData* data = state->GetEffectData<OscData>();
+
+    float phase = data->phase;
+    float srate = state->samplerate;  
+    float freq = data->freq;
+
+    //how much phase to add per sample increment
+    float phaseIncrement = freq / srate;
+
+    //for the length of the buffer, calculate the current sample to send, increment phase, and if phase goes over 1, reset it to 0
+    for (unsigned int n = 0; n < length; ++n)
+    {
+        float current_sample = sinf(phase * 2.0f * M_PI);
+
+        phase += phaseIncrement;
+        if (phase >= 1.0f)
+            phase -= 1.0f;
+
+        for (int ch = 0; ch < outchannels; ++ch)
+            outbuffer[n * outchannels + ch] = current_sample;
+    }
+
+    data->phase = phase;
+
+    return UNITY_AUDIODSP_OK;
+}
+
+//clean-up
+UNITY_AUDIODSP_RESULT UNITY_AUDIODSP_CALLBACK ReleaseCallback(UnityAudioEffectState* state){
+    OscData* data = state->GetEffectData<OscData>();
+    delete data;
+    return UNITY_AUDIODSP_OK;
+}
--- audioplugin/build-android/cmake_install.cmake
+++ audioplugin/build-android/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/desmo/Documents/UnityVRTheremin
+# Install script for directory: C:/Users/desmo/Documents/UnityVRTheremin/audioplugin
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/testlib")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/UnityOscillator")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/desmo/Documents/UnityVRTheremin/build-android/install_local_manifest.txt"
+  file(WRITE "C:/Users/desmo/Documents/UnityVRTheremin/audioplugin/build-android/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/desmo/Documents/UnityVRTheremin/build-android/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/desmo/Documents/UnityVRTheremin/audioplugin/build-android/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
